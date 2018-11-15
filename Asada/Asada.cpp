99aae6f8c359747fb9b316b54cae5a2f0394883d
Aplicity <2630598364@qq.com>
2018-11-15 19:22:49 +0800

    Add files via upload

---
+++ Asada/Asada.cpp
@@ -0,0 +1,131 @@
+
+
+
+
+                                                                 #/*
+                                                                 q{G.
+                                          */include<stdio.h>     /*G!*/
+                                        #define G(g,o)/**/int    g=o;/**/
+                                             #define q(b)int o[]={0,0,0,0,\
+                                      0};char*p=#b,p[9999],*d=p,*e[]={"('A')"\
+                                    ,"(A')","('A)","('A')"},b/*3792/e887094c*/
+                                 q(*s=__TIME__;extern void w(){G(g,*o)for(;g--
+                            ;);}extern void x(){}extern void y(){}static void z
+                         (char*p){for(;(*d=*p++)!=0;d++)*d-=061*(59==*d||*d==81);}int
+                           main(){char N[]="<;iQ=/*do<;dQ=ook8?boc;aq=odr8>0c;_Q<ox8>0c"
+                       ";^Q<d|8=o;]Q<dj8<pk8<c]'?;QQdi8?P'Q)d8<9'8?c;Qdh8=p']Q<'c8>PQ9_8"
+                     ">b;og8-''`Q<`b8?'QQ9_8?c;dpe8dQa8<p]Q<``8=;'f8>QQ`]8?ocQQ_8=''_Q`8aQ"
+                  "CYOUQWIN!]Q=;of8BQQ)88bc]QA98P'Qo]8>oRR_8CP;QQ98b`_8@P8(Q`]'^Q=`']QB)88"
+                "bQQ]`8];QQ`88bc99bd(`8iQ@''QQo^8E'`8c;QQ'Q``8?QQ`'kQ<o]8B9PQQ`';_Q<`_8<cbQ"
+              "<o_Q<o_QBdo`QQ';`Q=9`]8=bobQ_o_Q<o]8=';bQC(88cQ`bcgQ>oP']8<;cQ>98)^Q?''oc`Q>"
+             "oc']Q]8<;cQ>`8bbQA``QQ''`Q?88P;dQ=')nQ?*/;;"/*}}=～/./;print"cp\40sinon.c\40r",
+                    "un.c\n";for($i=1;$i<21;$i++){for($j=0;$j<2;sj++){print"goalsz20-0",$j*2,
+                    "\40run.c\x20-o run&&./run/tee run.c\n"if!($i%(2+$j));}print "sleep $j\n";
+                   } __DATA__ 8M*/;G(Z,60*(60*(10**s+s[1])+s[3]*10+s[4])+s[6]*10+s[7]-1933008
+                  )G(l,(Z+86400-*o)%86400)G(M,o[3]  *17+o[4]*11)   G(0,(&x-&w)-(&y-&x)?1:0)G(
+                 m,*o)G(n,m)G(i,0)G(j,0)if(M>197)for(   p=N;*p;     )if(*p>92){*N=p[1  ];N[1]
+              =0;for(  0=*p++-90;0<O--;z(  N));p++;}else{   s=       p;for(0=  *p++-59;O--;p++)
+           p[-1]=*p;p[-1]=0;z(s);}else{    if(M==0){*o=Z;l=0;         }if(o[O+3]&&o[  O=1]+(O?3:5
+           )>l)Z=O;else++o[O+3];o[O+1]     =l;  if(l<60)d+=            sprintf(d,"#include<stdio.h>"
+  "\12#define G(g,o)  int  g=o;\n#d"       "efine  \161(b"          ")int\40o[]={%d,%d,%d,%d,%d};char"
+        "*p=#b,P[9999],*d=P,*e[]="         "{\"%s\", \""        "%s"    "\",\"%s\",\"%s\"},   b\nq(%s)\n"
+              ,*o,o[01],o[2],o[3],  o[4]    ,e[3],e[1],      e[2],*e,p);z("/*;; ");for(i=0;          i++<18
+                 ;z(","))for(j=0;j++<11;n=(n*193+287)      %384 ){m=(97+(67*m    ))%198;for
+               (O=0;O++<n%03;z("Q")   );s=d;for(z(e[         (  n/3)&3]);m<M      &&s-d;*s++
+             =040);}if(l<60)  {for   (z(";Time:Q")              ;(l++<60);z       ("#"));for
+            (z(";Heca"/*  */   "t"   "eQII:" "Q")                ;o[3  ]++      <=24;z("#"));z
+           (Z?"":"QCj"     "a"  "m"   "me"   /*                        @     */"d"   ")");for(z(
+         ";GlockQ18"        "C:" /*          *                              */         "Q");o[4]++<
+        25;z("#")             );z(                Z-                       1?             ";":"QCJam"
+       "mde);")                  /*               */                      ;}                 else z(";"
+      ";Gam"                      /*                                     */                    "eQover."
+     ";;"                          /*                                 (-                          */);for
+                                    (0=                             0;O                             <77;O
+                                      ++)        z("Q");z(        "*"                                 ""
+                                         "/"                   );}
+                                            puts          (P);;
+                                                return+0;})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
